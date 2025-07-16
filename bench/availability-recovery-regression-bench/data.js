f98a82af467a42b1c4c8733ea15e5f7aa02d31d6
github-action-benchmark <github@users.noreply.github.com>
2025-07-16 17:31:03 +0000

    add availability-recovery-regression-bench (customSmallerIsBetter) benchmark result for 40e1a2a7c99c67fe5201145e473c87e1aea4bf05

--- bench/availability-recovery-regression-bench/data.js
+++ bench/availability-recovery-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1752652673410,
+  "lastUpdate": 1752687063331,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "availability-recovery-regression-bench": [
@@ -64187,6 +64187,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 0.20058436279999997,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bkontur@gmail.com",
+            "name": "Branislav Kontur",
+            "username": "bkontur"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "40e1a2a7c99c67fe5201145e473c87e1aea4bf05",
+          "message": "Allow create backport branches to unstable by A4-backport-unstable* tag (#9167)\n\nIn this\n[PR](https://github.com/paritytech/polkadot-sdk/pull/9139#issuecomment-3052828167),\nI added the `A4-backport-unstable2507` label, but no backport branch was\ncreated for `unstable2507`.\n\nWas this intentional or just an oversight or did I miss anything in the\nrelease channel?\nHow do we do backports to unstable2507? If manually, just close this.\n\ncc: @EgorPopelyaev - this PR is just a blind draft (not sure if it\nworks), probably more needs to be fixed and properly tested. If we\nreally need this, could you please take it over the finish line? If not,\njust close it :)\n\nCo-authored-by: Egor_P <egor@parity.io>",
+          "timestamp": "2025-07-16T16:23:51Z",
+          "tree_id": "42f9ddd2a1ed680cc694879a7f84761b03ea1e9c",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/40e1a2a7c99c67fe5201145e473c87e1aea4bf05"
+        },
+        "date": 1752687046024,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 1.6666666666666665,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 307203,
+            "unit": "KiB"
+          },
+          {
+            "name": "availability-recovery",
+            "value": 11.268504478133337,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.19565968156666674,
             "unit": "seconds"
           }
         ]
